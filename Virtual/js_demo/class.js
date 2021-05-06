f6cf022e356424f6815d7a3b1b28ce9194572ea9
Anthony-Tag <anthony.escalante@revature.net>
2021-05-06 11:10:20 -0400

    5/6/21

---
+++ Virtual/js_demo/class.js
@@ -0,0 +1,11 @@
+class Animal {
+    constructor(type, age, name){
+        this.type = type;
+        this.age = age;
+        this.name = name;
+    }
+    var name;
+}
+class fish extends Animal {
+
+}
