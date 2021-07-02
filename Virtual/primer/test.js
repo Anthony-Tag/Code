241828401d800a601009871224ede8f06dae1971
Anthony-Tag <anthony.escalante@revature.net>
2021-07-02 18:00:40 -0400

    7/2/21

---
+++ Virtual/primer/test.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    document.getElementById("myPara").innerHTML = "My First JavaScript";
+
+    alert("Help they have me!")
+
+    console.log("test")
+}
+
+function name(param1, param2) {
+    console.log(param1 + param2);
+}
+// function name(param1){
+//     console.log(param1);
+// }
+
+function fToc() {
+    var f = document.getElementById("fname").value;
+    f = (5 / 9) * (f - 32);
+
+    document.getElementById("results").innerHTML = f;
+}
+
+"use strict";
+
+x = 3.14
+
+"use strict";
+
+function statement() {
+
+    let x, y, z;
+
+    x = 5;
+    y = 8;
+    z = x + y;
+
+    console.log(typeof (x));
+    document.getElementById("mother").innerHTML = "hola"
+}
+
